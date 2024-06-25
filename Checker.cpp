fd5a05ca420b7a76cb8a18ac9365e934b02aaef6
SUJAYRUDRESH <60745093+SUJAYRUDRESH@users.noreply.github.com>
2024-06-25 22:45:13 +0530

    First Commit Update Checker.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -1,22 +1,89 @@
 #include <assert.h>
 #include <iostream>
+
+
 using namespace std;
 
-bool batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
-    cout << "Temperature out of range!\n";
-    return false;
-  } else if(soc < 20 || soc > 80) {
-    cout << "State of Charge out of range!\n";
-    return false;
-  } else if(chargeRate > 0.8) {
-    cout << "Charge Rate out of range!\n";
-    return false;
-  }
-  return true;
+class BatteryManagementSystem {
+public:
+    bool isTemperatureInRange(float temperature) const;
+    bool isSocInRange(float soc) const;
+    bool isChargeRateInRange(float chargeRate) const;
+    bool batteryIsOk(float temperature, float soc, float chargeRate) const;
+
+private:
+    void logMessage(const std::string& message) const;
+};
+
+
+void BatteryManagementSystem::logMessage(const std::string& message) const {
+    // Log the message to console
+    cout << message << endl;
+    
+}
+
+bool BatteryManagementSystem::isTemperatureInRange(float temperature) const {
+    if (temperature < 0 || temperature > 45) {
+        logMessage("Temperature out of range!");
+        return false;
+    }
+    return true;
+}
+
+bool BatteryManagementSystem::isSocInRange(float soc) const {
+    if (soc < 20 || soc > 80) {
+        logMessage("State of Charge out of range!");
+        return false;
+    }
+    return true;
+}
+
+bool BatteryManagementSystem::isChargeRateInRange(float chargeRate) const {
+    if (chargeRate > 0.8) {
+        logMessage("Charge Rate out of range!");
+        return false;
+    }
+    return true;
+}
+
+bool BatteryManagementSystem::batteryIsOk(float temperature, float soc, float chargeRate) const {
+    return isTemperatureInRange(temperature) && isSocInRange(soc) && isChargeRateInRange(chargeRate);
+}
+
+
+
+void testIsTemperatureInRange() {
+    BatteryManagementSystem bms;
+    assert(bms.isTemperatureInRange(25) == true);
+    assert(bms.isTemperatureInRange(-5) == false);
+    assert(bms.isTemperatureInRange(50) == false);
+}
+
+void testIsSocInRange() {
+    BatteryManagementSystem bms;
+    assert(bms.isSocInRange(50) == true);
+    assert(bms.isSocInRange(10) == false);
+    assert(bms.isSocInRange(90) == false);
+}
+
+void testIsChargeRateInRange() {
+    BatteryManagementSystem bms;
+    assert(bms.isChargeRateInRange(0.7) == true);
+    assert(bms.isChargeRateInRange(0.9) == false);
+}
+
+void testBatteryIsOk() {
+    BatteryManagementSystem bms;
+    assert(bms.batteryIsOk(25, 70, 0.7) == true);
+    assert(bms.batteryIsOk(50, 85, 0) == false);
 }
 
 int main() {
-  assert(batteryIsOk(25, 70, 0.7) == true);
-  assert(batteryIsOk(50, 85, 0) == false);
+    testIsTemperatureInRange();
+    testIsSocInRange();
+    testIsChargeRateInRange();
+    testBatteryIsOk();
+    std::cout << "All tests passed!" << std::endl;
+    return 0;
 }
+
