6c46cfda09a11955718860ed632fcd0ace0517ba
SUJAYRUDRESH <60745093+SUJAYRUDRESH@users.noreply.github.com>
2024-07-25 23:31:01 +0530

    Create BatteryChecker.h

---
+++ BatteryChecker.h
@@ -0,0 +1,23 @@
+#ifndef BATTERYCHECKER_H
+#define BATTERYCHECKER_H
+
+#include "LanguageManager.h"
+#include "StatusManager.h"
+
+class BatteryChecker {
+public:
+    BatteryChecker(LanguageManager& langMgr, StatusManager& statusMgr)
+        : languageManager(langMgr), statusManager(statusMgr) {}
+
+    void checkBatteryStatus(float temperature, float soc, float chargeRate);
+
+private:
+    LanguageManager& languageManager;
+    StatusManager& statusManager;
+
+    Status mapToStatus(float value, const std::vector<Status>& statuses) const;
+    Status getOverallStatus(Status temperatureStatus, Status socStatus, Status chargeRateStatus) const;
+    void outputMessage(const Status& status) const;
+};
+
+#endif // BATTERYCHECKER_H
