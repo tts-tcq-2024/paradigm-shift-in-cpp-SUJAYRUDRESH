163ba1b7832f920934df5a06c665e03be1e0ca16
SUJAYRUDRESH <60745093+SUJAYRUDRESH@users.noreply.github.com>
2024-07-26 00:06:41 +0530

    Create TestBatteryChecker.h

---
+++ TestBatteryChecker.h
@@ -0,0 +1,13 @@
+#ifndef TESTBATTERYCHECKER_H
+#define TESTBATTERYCHECKER_H
+
+#include <cassert>
+#include <iostream>
+#include "LanguageManager.h"
+#include "StatusManager.h"
+#include "BatteryChecker.h"
+
+void testStatusManager();
+void testBatteryChecker();
+
+#endif // TESTBATTERYCHECKER_H
