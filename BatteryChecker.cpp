29861d2192d958d782f4250cf6643b9d72db4485
SUJAYRUDRESH <60745093+SUJAYRUDRESH@users.noreply.github.com>
2024-08-02 07:10:25 +0530

    Update BatteryChecker.cpp

--- BatteryChecker.cpp
+++ BatteryChecker.cpp
@@ -1,33 +1,31 @@
 #include <iostream>
 #include "BatteryChecker.h"
 
-Status BatteryChecker::mapToStatus(float value, const std::vector<Status>& statuses) const {
+BatteryParameterInfo BatteryChecker::mapToStatus(float value, const std::vector<BatteryParameterInfo>& statuses) const {
     for (const auto& status : statuses) {
         if (isValueInRange(value, status)) {
             return status;
         }
     }
-    return Status(Parameter::SOC, "NORMAL", 0, 0);  // Default to NORMAL if no range matched
+    return BatteryParameterInfo(Parameter::SOC, "NORMAL", 0, 0);  // Default to NORMAL if no range matched
 }
 
-bool BatteryChecker::isValueInRange(float value, const Status& status) const {
+bool BatteryChecker::isValueInRange(float value, const BatteryParameterInfo& status) const {
     return value >= status.getMin() && value <= status.getMax();
 }
 
-Status BatteryChecker::getOverallStatus(Status temperatureStatus, Status socStatus, Status chargeRateStatus) const {
-    // Logic to determine overall status; for simplicity, we assume any breach results in a critical status.
-    //bool hasHighStatus = temperatureStatus.getName() == "HIGH_TEMP_BREACH" || socStatus.getName() == "HIGH_SOC_BREACH" || socStatus.getName() == "HIGH_SOC_BREACH" ;
+BatteryParameterInfo BatteryChecker::getOverallStatus(BatteryParameterInfo temperatureStatus, BatteryParameterInfo socStatus, BatteryParameterInfo chargeRateStatus) const {
     bool highStatus = hasHighStatus(temperatureStatus, socStatus, chargeRateStatus);
-    return highStatus ? Status(Parameter::SOC, "HIGH", 0, 0) : Status(Parameter::SOC, "NORMAL", 0, 0);
+    return highStatus ? BatteryParameterInfo(Parameter::SOC, "HIGH", 0, 0) : BatteryParameterInfo(Parameter::SOC, "NORMAL", 0, 0);
 }
 
-bool BatteryChecker::hasHighStatus(const Status& temperatureStatus, const Status& socStatus, const Status& chargeRateStatus) const {
+bool BatteryChecker::hasHighStatus(const BatteryParameterInfo& temperatureStatus, const BatteryParameterInfo& socStatus, const BatteryParameterInfo& chargeRateStatus) const {
     return temperatureStatus.getName() == "HIGH_TEMP_BREACH" ||
            socStatus.getName() == "HIGH_SOC_BREACH" ||
            chargeRateStatus.getName() == "HIGH_CHARGE_RATE_BREACH";
 }
 
-void BatteryChecker::outputMessage(const Status& status) const {
+void BatteryChecker::outputMessage(const BatteryParameterInfo& status) const {
     std::string message = statusManager.getMessage(status, languageManager.getLanguage());
     std::cout << message << std::endl;
 }
@@ -37,14 +35,10 @@ void BatteryChecker::checkBatteryStatus(float temperature, float soc, float char
     auto socStatuses = statusManager.getStatuses(Parameter::SOC);
     auto chargeRateStatuses = statusManager.getStatuses(Parameter::CHARGE_RATE);
 
-    Status tempStatus = mapToStatus(temperature, temperatureStatuses);
-    Status socStatus = mapToStatus(soc, socStatuses);
-    Status chargeRateStatus = mapToStatus(chargeRate, chargeRateStatuses);
+    BatteryParameterInfo temperatureStatus = mapToStatus(temperature, temperatureStatuses);
+    BatteryParameterInfo socStatus = mapToStatus(soc, socStatuses);
+    BatteryParameterInfo chargeRateStatus = mapToStatus(chargeRate, chargeRateStatuses);
 
-    outputMessage(tempStatus);
-    outputMessage(socStatus);
-    outputMessage(chargeRateStatus);
-
-    Status overallStatus = getOverallStatus(tempStatus, socStatus, chargeRateStatus);
-    std::cout << statusManager.getMessage(overallStatus, languageManager.getLanguage()) << std::endl;
+    BatteryParameterInfo overallStatus = getOverallStatus(temperatureStatus, socStatus, chargeRateStatus);
+    outputMessage(overallStatus);
 }
